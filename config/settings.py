9aee54148535b8fd0fcfd717f07a9e2a12c8f49d
bendamian <badamian@gmail.com>
2025-04-13 20:29:47 +0100

    add a video app

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'books',
     'gallery',
+    'video',
 ]
 
 MIDDLEWARE = [
--- gallery/views.py
+++ gallery/views.py
@@ -5,14 +5,14 @@ from django.core.paginator import Paginator
 
 
 def album_list(request):
-    #albums = Album.objects.all()
     albums = Album.objects.all().order_by('-created_at')
-    # Set up pagination
     paginator = Paginator(albums, 6)  # Show 6 albums per page
     page_number = request.GET.get('page')
     album_page = paginator.get_page(page_number)
-    return render(request, 'gallery/album_list.html', {'albums': albums, 'album_page': album_page})
+    return render(request, 'gallery/album_list.html', {'album_page': album_page})
+
+
 
 def album_detail(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'gallery/album_detail.html', {'album': album_list})
+    return render(request, 'gallery/album_detail.html', {'album': album})
